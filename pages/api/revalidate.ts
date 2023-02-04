02f22b22642e96807127d26bc70180680ef33cb0
Alex Traher <alex.traher@gmail.com>
2023-02-04 15:21:47 +0000

    fixed validate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -15,10 +15,6 @@ const revalidate: NextApiHandler = async (req, res) => {
   }
 
   try {
-    // Note: if this fails to parse you may have forget to set the
-    // "content-type" header correctly as mentioned here https://github.com/vercel/next.js/blob/canary/examples/cms-contentful/README.md#step-9-try-using-on-demand-revalidation
-    let postSlug = req.body.fields.slug['en-US']
-
     await res.revalidate('/')
 
     return res.json({ revalidated: true })
